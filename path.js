9847fe1de0854bc71ce03c45bfeab2e3265f3fc9
alliejj99 <118407356+alliejj99@users.noreply.github.com>
2023-03-31 22:34:29 +0900

    WebpackLoader 설정

--- path.js
+++
@@ -1,4 +0,0 @@
-const path = require("path"); // node.js에서 제공하는 모듈입니다.
-
-console.log(path.resolve(__dirname, "src/index.js")); // 시작하는 파일을 찾아서 확인이 가능합니다.
-// 터미널에서 node path.js를 치면 위의 내용을 확인할 수 있습니다.
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  // 시작점
+  entry: {
+    main: path.resolve(__dirname, "src/index.js"),
+  },
+  // 웹팩 작업을 통해 생성된 결과물
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss?$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+};
